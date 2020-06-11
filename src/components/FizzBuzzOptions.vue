3dd83c0fe5165edde43817b43d2f64bc2793660d
drewbitt <andrew@drewbitt.com>
2020-06-11 12:53:11 -0400

    Begin styling main page

---
+++ src/components/FizzBuzzOptions.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="fizzBuzzOptions">
+    <p class="instructions">
+      Please enter a fizz and buzz time in seconds.
+      <strong>Values should be 2 to 10, inclusive.</strong>
+    </p>
+    <div class="fizzBuzzOptionsForms">
+      <label for="fizz">Fizz:</label>
+      <input id="fizz" name="fizz" type="text" />
+      <label for="buzz">Buzz</label>
+      <input id="buzz" name="buzz" type="text" />
+      <button>Go to timer ></button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+<style scoped>
+.fizzBuzzOptions {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.fizzBuzzOptionsForms {
+  margin-top: 40px;
+}
+
+label {
+  font-size: 18px;
+  margin-right: 5px;
+}
+
+.instructions {
+  font-size: 30px;
+}
+
+input {
+  width: 100px;
+  padding: 8px 16px;
+  border: 1px solid #707070;
+  background-color: #f5f5f5;
+  border-radius: 5px;
+  font-size: 16px;
+  margin-right: 40px;
+}
+
+button {
+  margin-top: 50px;
+  border: 2px solid #707070;
+  color: #707070;
+  padding: 15px 30px;
+  text-align: center;
+  font-size: 16px;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 5px;
+}
+</style>
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,6 @@
 // For fixing IDE errors when importing .vue files
-declare module "*.vue" {
-    import { defineComponent } from "vue";
-    const Component: ReturnType<typeof defineComponent>;
-    export default Component;
-}
+declare module '*.vue' {
+    import { Component } from 'vue'
+    const _default: Component
+    export default _default
+  }
